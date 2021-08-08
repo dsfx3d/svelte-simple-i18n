93edf605fb20338369f44f60cb0a37f710a275f7
dsfx3d <dsfx3d@gmail.com>
2021-08-08 15:53:33 +0530

    :tada: Initial commit

---
+++ src/lib/i18n.ts
@@ -0,0 +1,47 @@
+import _get from 'lodash.get';
+import { derived, readable, writable } from 'svelte/store';
+
+import type { Options } from '../types/i18n';
+
+export const createI18n = (options: Options) => {
+  const locale = writable(options.locale);
+  const fallbackLocale = readable(options.fallbackLocale);
+
+  const t = derived(
+    [locale, fallbackLocale],
+    ([$locale, $fallbackLocale]) =>
+      (path) => {
+        const messages = options.messages;
+        let locale = messages[$locale];
+
+        if (!($locale in messages)) {
+          console.warn(
+            `[i18n] locale "${$locale}" not found. Will use fallback locale "${$fallbackLocale}"`
+          );
+
+          locale = messages[$locale];
+        }
+
+        let text = _get(locale, path);
+
+        if (text === undefined && $locale in messages) {
+          console.warn(
+            `[i18n] "${path}", message not found is local "${$locale}". Will use fallback locale "${$fallbackLocale}".`
+          );
+
+          text = _get(messages[$fallbackLocale], path);
+        }
+
+        if (text === undefined) {
+          console.warn(
+            `[i18n] "${path}", message not found in fallback locale either.`
+          );
+          return '';
+        }
+
+        return text;
+      }
+  );
+
+  return { t, locale };
+};
---
+++ src/types/i18n.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type Locale = string;
+
+export type Messages = {
+  [key: string]: any;
+};
+
+export type Options = {
+  messages: Messages;
+  locale: Locale;
+  fallbackLocale: Locale;
+};
